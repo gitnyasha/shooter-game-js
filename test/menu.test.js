d8c7fafaac108ba51cf96031370f932c638713ea
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-23 18:17:01 +0200

    lintor errors

--- test/menu.test.js
+++ test/menu.test.js
@@ -1,48 +1,48 @@
-import "jest-canvas-mock";
-import Game from "../src/Scenes/GameInit";
+import 'jest-canvas-mock';
+import Game from '../src/Scenes/GameInit';
 
-describe("The game should", () => {
+describe('The game should', () => {
   let game;
   beforeAll(() => {
     game = new Game();
   });
 
-  test("have a width of 800", () => {
+  test('have a width of 800', () => {
     expect(game.config.width).toEqual(800);
   });
 
-  test("have a height of 600", () => {
+  test('have a height of 600', () => {
     expect(game.config.height).toEqual(600);
   });
 
-  test("have the musicOn set to true", () => {
+  test('have the musicOn set to true', () => {
     expect(game.globals.model.musicOn).toEqual(true);
   });
 
-  test("have an Object of scenes", () => {
-    expect(typeof game.scene._pending).toEqual("object");
+  test('have an Object of scenes', () => {
+    expect(typeof game.scene._pending).toEqual('object');
   });
 
-  test("have an Object of 6 scenes", () => {
+  test('have an Object of 7 scenes', () => {
     expect(game.scene._pending.length).toEqual(7);
   });
 });
 
-describe("Player", () => {
+describe('Player', () => {
   let game;
   beforeAll(() => {
     game = new Game();
   });
 
-  test("score should be 0 when starting the game", () => {
+  test('score should be 0 when starting the game', () => {
     expect(game.globals.playerScore.player_score).toEqual(0);
   });
 
-  test("score array should be empty when starting the game", () => {
+  test('score array should be empty when starting the game', () => {
     expect(game.globals.playerScore.scores.length).toEqual(0);
   });
 
-  test("score should be updated", () => {
+  test('score should be updated', () => {
     let i = 0;
     while (i < 8) {
       game.globals.playerScore.updateScore();
@@ -51,7 +51,7 @@ describe("Player", () => {
     expect(game.globals.playerScore.player_score).toEqual(80);
   });
 
-  test("score should be reseted after submiting the score", () => {
+  test('score should be reseted after submiting the score', () => {
     game.globals.playerScore.resetScore();
     let i = 0;
     while (i < 5) {
@@ -59,7 +59,7 @@ describe("Player", () => {
       i += 1;
     }
     expect(game.globals.playerScore.player_score).toEqual(50);
-    game.globals.playerScore.submitScore("Yair");
+    game.globals.playerScore.submitScore('Yair');
     expect(game.globals.playerScore.player_score).toEqual(0);
   });
 });
